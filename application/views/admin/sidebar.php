55a4fe9f19cdff360423dc138405cf8773e0e04e
thecrown <amienkurniawan5@gmail.com>
2017-12-19 22:26:40 +0700

    19 des 2017

--- application/views/admin/sidebar.php
+++ application/views/admin/sidebar.php
@@ -140,6 +140,17 @@
            <li><a href="<?php echo base_url('comment');?>"><i class="fa fa-circle-o"></i> View All Comment</a></li>  
           </ul>
         </li>
+        <li class="treeview">
+          <a href="#">
+              <i class="fa fa-users"></i> <span>Pengaturan User</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">          
+           <li><a href="<?php echo base_url('UsersView');?>"><i class="fa fa-circle-o"></i> View All Users</a></li>  
+          </ul>
+        </li>
            
            
       </ul>
--- mwa/application/views/admin/filesize.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$a = "assets/uploaded_files/attachments/181017-amien.jpg";
-echo number_format(filesize($a) / 1048576, 2) . ' MB';
-?>
